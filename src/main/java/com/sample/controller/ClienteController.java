e2cbed7c044d03e1314f81f002f52bff09859bb3
Celio Junior <celio_cjr@hotmail.com>
2022-04-26 01:00:54 -0300

    API finalizada.

---
+++ src/main/java/com/sample/controller/ClienteController.java
@@ -0,0 +1,29 @@
+package com.sample.controller;
+
+import com.sample.model.Cliente;
+import com.sample.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @GetMapping
+    public List<Cliente> listar() {
+        return clienteRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+}
